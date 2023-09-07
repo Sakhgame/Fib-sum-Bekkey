9561a3362b4f97be4a9046fdcbcea10b7819c793
User <User@NPK-208-11>
2023-09-07 11:58:48 +0700

    Добавьте файлы проекта.

---
+++ Fib-sum/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more informationi
+int N= int.Parse(Console.ReadLine());
+int s = 0;
+int f1 = 1;
+int f2 = 1;
+int c;
+for (int i = 1; i < N; i++)
+{
+    s = s + f1;
+    c = f1;
+    f1 = f2;
+    f2 = c + f2;
+}
+Console.WriteLine(s);
+
